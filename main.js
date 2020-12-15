18509d759fa91a83b148b7f9c6658d1fdf74877d
andreamazza84 <andrea.mazza84@gmail.com>
2020-12-15 14:42:53 +0100

    Frecce funzionanti

--- main.js
+++ main.js
@@ -9,3 +9,31 @@
 // cliccando sulle frecce della tastiera sinista e destra scorriamo tra le immagini
 // I bonus metteteli in una cartella "bonus".
 
+let app = new Vue({
+    el: '.container',
+    data: {
+            images: [
+                "https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg",
+                "https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg",
+                "https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg",
+                "https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg"
+            ],
+            counter: 0,
+    },
+    methods: {
+        next(){
+            this.counter++;
+            if(this.counter === this.images.length){
+                this.counter = 0;
+            }
+            return this.counter;
+        },
+        prev(){
+            this.counter--;
+            if(this.counter < 0){
+                this.counter = this.images.length - 1;
+            }
+            return this.counter;
+        },
+    },
+});
